0b9bd8423f8127998b2fc63875278294de280e94
Joel Cutler <joelcutler108@gmail.com>
2026-05-25 21:23:59 +0100

    create script plan

---
+++ main.ps1
@@ -0,0 +1,17 @@
+param(
+    $DeleteRepo,
+    $MonoRepo
+)
+
+# check that git and git filter tools is installed
+
+# create temporary directory
+
+# clone both repositories, don't actually need data...
+
+# move deleting repository to a subfolder
+
+# merge into monorepo
+
+# probably user confirmation, then push monorepo and delete
+
