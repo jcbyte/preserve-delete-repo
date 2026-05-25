12045a4953568959cc2bb786b56542daac98911b
Joel Cutler <joelcutler108@gmail.com>
2026-05-25 21:34:55 +0100

    check that commands are available

--- main.ps1
+++ main.ps1
@@ -5,6 +5,24 @@ param(
 
 Import-Module (Join-Path $PSScriptRoot "Util")
 
+if (-not (Get-Command "git" -ErrorAction SilentlyContinue)) {
+  Write-Warning "Git was not found, please install it first"
+  exit 1
+}
+
+if (-not (Get-Command "python" -ErrorAction SilentlyContinue)) {
+  Write-Warning "Python was not found, please install it first"
+  exit 1
+}
+
+python -m git_ilter_repo --version | Out-Null
+if (-not $?) {
+  Write-Warning "git-filter-repo was not found, please install it first"
+  Write-Host "run: " -NoNewline
+  Write-Host "python -m pip install git-filter-repo" -ForegroundColor Cyan
+  exit 1
+}
+
 # check that git and git filter tools is installed
 
 # create temporary directory
